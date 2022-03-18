9258311574e05b58dde6f59510fbd3f16cdb09e5
user <thestrongest017@gmail.com>
2022-03-18 12:00:56 +0600

    HW--29

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.function.*;
 
 public class Main {
 
-    /*sqrt деген метод тузунуз.
-UnaryOperator деген интерфейстин реализациясын кайтаруусу керек
-Бир санды кабыл алып анын тамырдан чыккан жообун кайтарат(корень).
-
-public static UnaryOperator<Double> sqrt().*/
+    /*Integer массивден жуп сандарды терип алынызда алардын квадраттарынын эн чонун табыныз.
+     */
 
     public static void main(String[] args) {
 
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            list.add(random.nextInt(10));
+        }
+        System.out.println(list);
+        System.out.println();
+        list.stream().filter(x -> x % 2 == 0).map(x -> x*x).max(Integer:: compare).ifPresent(System.out::println);
 
-        System.out.println(sqrt.apply(30d));
+
     }
 
-    public static UnaryOperator<Double> sqrt = Math :: sqrt;
+
 
 
 
